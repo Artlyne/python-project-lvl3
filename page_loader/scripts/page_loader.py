49dffe7ad14788fc6d445e52b0887a91bd172286
Andrey Martsenius <artlynee@gmail.com>
2021-04-08 22:47:51 +0500

    feat: add network and OS error handling

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-from page_loader import page_loader, cli
+import requests
+import sys
+from page_loader import app_logger, page_loader, cli
+
+logger = app_logger.get_logger(__name__)
 
 
 def main():
     args = cli.get_args().parse_args()
-    result = page_loader.download(args.url, args.output)
-    print(result)
+    try:
+        result = page_loader.download(args.url, args.output)
+        print(result)
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        sys.exit(1)
+    except OSError as e:
+        logger.error(e)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
