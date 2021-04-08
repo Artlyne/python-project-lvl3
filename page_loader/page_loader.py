8137672229178dbfdf0fcd3ea69b6cb40eabed89
Andrey Martsenius <artlynee@gmail.com>
2021-04-08 13:37:06 +0500

    feat: move the logger to a separate module and adding more logs

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,29 +1,31 @@
-import logging
 import os
 import sys
 import requests
-from page_loader import resources, naming
+from page_loader import app_logger, resources, naming
+
+logger = app_logger.get_logger(__name__)
 
 
 def download(url: str, path='') -> str:
-    logging.basicConfig(filename='app.log', filemode='w', level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - '
-                               '%(message)s - %(filename)s - '
-                               'function %(funcName)20s()'
-                        )
-    try:
-        response = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        logging.error(f"Connection Error. Can't connect to: {url}")
-        sys.exit()
-    logging.info('making file name')
-    path_to_page = os.path.join(path, naming.create(url))
-    path_to_files = path_to_page.rstrip('.html') + '_files'
-    logging.info(f'Creating directory {path_to_files}')
-    os.makedirs(path_to_files, exist_ok=True)
-    logging.info('changing links to local resources')
-    page = resources.replace(url, response.text, path_to_files)
-    with open(path_to_page, 'w', encoding='utf-8') as file:
-        logging.info('writing to file')
-        file.write(page)
-    return path_to_page
+    logger.info(f'getting response from {url}')
+    response = requests.get(url)
+
+    logger.info(f'creating name for {url}')
+    filename = naming.create(url)
+    logger.info(f'creating path for {filename}')
+    htmlpage_path = os.path.join(path, filename)
+    logger.info('creating path for assets')
+    assets_path = htmlpage_path.rstrip('.html') + '_files'
+
+    logger.info(f'creating directory {assets_path}')
+    os.makedirs(assets_path, exist_ok=True)
+
+    logger.info('replacing page content to local')
+    page_content = resources.replace_to_local(url, response.text, assets_path)
+
+    logger.info(f'writing page content to {htmlpage_path}')
+    with open(htmlpage_path, 'w', encoding='utf-8') as file:
+        file.write(page_content)
+
+    logger.info('Function done! Returning the path to the page.')
+    return htmlpage_path
--- page_loader/resources.py
+++ page_loader/resources.py
@@ -1,43 +1,58 @@
-import logging
 import os
 import requests
-import sys
 from bs4 import BeautifulSoup
 from progress.bar import Bar
 from urllib.parse import urlparse, urljoin
-from page_loader import naming
+from page_loader import app_logger, naming
 
 
 TAGS = {'img': 'src', 'script': 'src', 'link': 'href'}
+logger = app_logger.get_logger(__name__)
 
 
 def is_valid(url: str, link: str) -> bool:
+
     if link is None:
         return False
+
     main_domain = urlparse(url).netloc
     file_domain = urlparse(link).netloc
     return main_domain == file_domain
 
 
 def download(url: str, path: str) -> str:
-    try:
-        response = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        logging.error(f"Connection Error. Can't connect to: {url}")
-        sys.exit()
-    path_to_file = os.path.join(path, naming.create(url))
-    with open(path_to_file, 'wb') as file:
+    logger.info(f'getting response from {url}')
+    response = requests.get(url)
+
+    logger.info(f'creating name for {url}')
+    filename = naming.create(url)
+    logger.info(f'creating path for {filename}')
+    filepath = os.path.join(path, filename)
+
+    logger.info(f'writing file content to {filepath}')
+    with open(filepath, 'wb') as file:
         file.write(response.content)
-    return path_to_file
+
+    logger.info('Function done! Returning the path to the file.')
+    return filepath
 
 
-def replace(url: str, page: str, path: str):
-    soup = BeautifulSoup(page, 'html.parser')
+def replace_to_local(url: str, htmlpage: str, assets_path: str):
+    logger.info('making the soup')
+    soup = BeautifulSoup(htmlpage, 'html.parser')
+
+    logger.info('looking for links')
     for resource in soup.findAll(TAGS):
         resource_source = TAGS[resource.name]
         link = urljoin(url, resource.get(resource_source))
+
+        logger.info('checking if resource is local')
         if is_valid(url, link):
+            logger.info(f'downloading {link}')
             Bar(f'Loading {link}\n')
-            path_to_file = download(link, path)
-            resource[resource_source] = path_to_file
+            filepath = download(link, assets_path)
+            logger.info(f'replacing resource path in page to local')
+            resource[resource_source] = filepath
+
+    logger.info('Function done! Returning the html page.')
     return soup.prettify(formatter='html5')
